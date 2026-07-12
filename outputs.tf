f8651fb935041fc10aac0081e25591172060960f
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:03 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "palo_alto_local_rulestack_fqdn_lists_id" {
+  description = "Map of id values across all palo_alto_local_rulestack_fqdn_lists, keyed the same as var.palo_alto_local_rulestack_fqdn_lists"
+  value       = { for k, v in azurerm_palo_alto_local_rulestack_fqdn_list.palo_alto_local_rulestack_fqdn_lists : k => v.id }
+}
 output "palo_alto_local_rulestack_fqdn_lists_audit_comment" {
   description = "Map of audit_comment values across all palo_alto_local_rulestack_fqdn_lists, keyed the same as var.palo_alto_local_rulestack_fqdn_lists"
   value       = { for k, v in azurerm_palo_alto_local_rulestack_fqdn_list.palo_alto_local_rulestack_fqdn_lists : k => v.audit_comment }
